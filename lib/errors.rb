365e0efc2a8ed6295311244d765184afc8eaa727
Tyler <tyler@tylermorgan.me>
2016-01-18 18:13:23 -0800

    Added chronic parsing

--- lib/errors.rb
+++ lib/errors.rb
@@ -1,3 +1,8 @@
 module UdaciListErrors
-  # Error classes go here
+  class InvalidItemType < StandardError
+  end
+  class IndexExceedsListSize < StandardError
+  end
+  class InvalidPriorityValue < StandardError
+  end
 end
--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -2,14 +2,25 @@ class UdaciList
   attr_reader :title, :items
 
   def initialize(options={})
-    @title = options[:title]
+    if options[:title] then
+      @title = options[:title]
+    else
+      @title = "Untitled List"
+    end
     @items = []
   end
   def add(type, description, options={})
     type = type.downcase
-    @items.push TodoItem.new(description, options) if type == "todo"
-    @items.push EventItem.new(description, options) if type == "event"
-    @items.push LinkItem.new(description, options) if type == "link"
+    case type
+    when "todo"
+      @items.push TodoItem.new(description, options)
+    when "event"
+      @items.push EventItem.new(description, options)
+    when "link"
+      @items.push LinkItem.new(description, options)
+    else
+      # This is where the error handler will go
+    end
   end
   def delete(index)
     @items.delete_at(index - 1)
