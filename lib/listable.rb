0129017200f42bde7c6447d2cadefbc893f19f85
Tyler <tyler@tylermorgan.me>
2016-01-18 17:41:25 -0800

    Add reformat_date to Listable and refactor out of event and todo

--- lib/listable.rb
+++ lib/listable.rb
@@ -3,4 +3,26 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+
+  def format_date(options = {})
+    if options[:due] then
+      dates = options[:due].strftime("%D")
+    elsif options[:start_date]
+      dates = options[:start_date].strftime("%D")
+      if options[:end_date] then
+        dates << " -- " + options[:end_date].strftime("%D")
+      end
+    else
+      dates = "N/A"
+    end
+    return dates
+  end
+
+  def format_priority()
+    value = " ⇧" if @priority == "high"
+    value = " ⇨" if @priority == "medium"
+    value = " ⇩" if @priority == "low"
+    value = "" if !@priority
+    return value
+  end
 end
--- lib/todo.rb
+++ lib/todo.rb
@@ -7,9 +7,6 @@ class TodoItem
     @due = options[:due] ? Date.parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
-  def format_date
-    @due ? @due.strftime("%D") : "No due date"
-  end
   def format_priority
     value = " ⇧" if @priority == "high"
     value = " ⇨" if @priority == "medium"
@@ -19,7 +16,7 @@ class TodoItem
   end
   def details
     format_description(@description) + "due: " +
-    format_date +
+    format_date(due: @due) +
     format_priority
   end
 end
